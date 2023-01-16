98a582e6033f577aaed581df5a94b5fffc8e675e
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-01-16 16:08:43 -0400

    fix to negative modulo for negative outcome

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -6,4 +6,4 @@ if number > 5:
 elif number == 0:
     print(f"Last digit of {number} is {number % 10} and is 0")
 elif number < 6 and number != 0:
-    print(f"Last digit of {number} is {abs(number) % 10} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {number % -10} and is less than 6 and not 0")
