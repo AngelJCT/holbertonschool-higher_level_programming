5ce4a094ca4086cec55675390b0d9ffb1a586263
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-01-16 17:19:11 -0400

    fixing long line

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -6,5 +6,5 @@ if number > 5:
 elif number == 0:
     print(f"Last digit of {number} is {number % 10} and is 0")
 elif number < 6 and number != 0:
-    print(f"Last digit of {number} is {(number % -10)} \
-        and is less than 6 and not 0")
+    print(f"Last digit of {number} is {(number % -10)}"\
+        " and is less than 6 and not 0")
