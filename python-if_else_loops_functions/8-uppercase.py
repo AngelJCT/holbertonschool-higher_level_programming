f6d567821506e2b8a5e40df9fe34f9f75d013285
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-01-17 09:25:54 -0400

    lowercase to uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,4 @@ def uppercase(str):
     for i in range(len(str)):
         if ord(str[i]) >= 97 and ord(str[i]) <= 122:
             print("{:c}".format(ord(str[i]) - 32), end="")
-        else:
-            print("{:c}".format(ord(str[i])), end="")
+        print("\n", end="")
