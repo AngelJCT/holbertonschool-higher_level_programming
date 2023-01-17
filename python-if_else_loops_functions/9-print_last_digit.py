7461b3fe98cfc7fdd8eae1ad9e8fc35e5dc60d9e
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-01-17 09:52:26 -0400

    last digit of a number

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last_digit = number % 10 # get last digit
-    if last_digit < 0: # if negative, make positive
-        last_digit *= -1
-    print("{:d}".format(last_digit), end="") # print last digit
-    return last_digit # return last digit
+    last_digit = number % 10  # get last digit
+    if last_digit < 0:  # if negative
+        last_digit = number % -10  # get last digit
+    print("{:d}".format(last_digit), end="")  # print last digit
+    return last_digit  # return last digit
