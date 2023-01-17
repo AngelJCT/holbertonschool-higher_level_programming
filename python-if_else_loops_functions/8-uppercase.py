45a7f278e11f4869e481944141bec220c922deb8
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-01-17 09:40:08 -0400

    lowercase to uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
-    new_str = ""
+    new_str = "" # new string to hold uppercase letters
     for i in range(len(str)):
-        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
-            new_str += chr(ord(str[i]) - 32)
+        if ord(str[i]) >= 97 and ord(str[i]) <= 122: # if lowercase
+            new_str += chr(ord(str[i]) - 32) # convert to uppercase by subtracting 32 from ASCII value and converting back to char
         else:
-            new_str += str[i]
-    print("{:s}".format(new_str), end="")
-    print()
+            new_str += str[i] # if not lowercase, just add to new string
+    print("{:s}".format(new_str), end="") # print new string without newline
+    print() # print new line
