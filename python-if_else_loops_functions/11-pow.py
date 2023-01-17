dd5fec9cc37688c9e116c10825717fdea0dbd702
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-01-17 19:28:32 -0400

    fixing code

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def pow(a, b):
-    my_base = a
-    my_exponent = b
-    my_result = 1
-    
-    while my_exponent != 0:
-        my_result *= my_base
-        my_exponent -= 1
-    return my_result
+    res = 1
+    for i in range(1, b + 1):
+        res *= a
+    return res
