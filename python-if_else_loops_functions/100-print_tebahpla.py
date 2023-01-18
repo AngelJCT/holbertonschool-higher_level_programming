1428682f225d1334ddd0208684e49e33b3078abb
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-01-17 21:38:05 -0400

    adding comments

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-counter = 0
-alphabet = ""
+counter = 0  # initialize counter
+alphabet = "" # initialize alphabet
 
-for i in range(ord('z'), ord('a') - 1, - 1):
-    c = chr(i)
-    if counter % 2 == 0:
-        alphabet = "{}".format(c.lower())
-    else:
-        alphabet = "{}".format(c.upper())
-    counter += 1
-    print(alphabet, end="")
+for i in range(ord('z'), ord('a') - 1, - 1):  # ord() returns the ASCII value of the character
+    c = chr(i)  # chr() returns the character of the ASCII value
+    if counter % 2 == 0: # if counter is even
+        alphabet = "{}".format(c.lower())  # lower() returns the lowercase version of the string
+    else:  # if counter is odd
+        alphabet = "{}".format(c.upper())  # upper() returns the uppercase version of the string
+    counter += 1  # increment counter
+    print(alphabet, end="")  # print alphabet without a new line
