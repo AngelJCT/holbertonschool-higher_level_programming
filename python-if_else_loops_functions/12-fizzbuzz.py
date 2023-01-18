c651f99a06ce4466addd049a0820a9c3121b4074
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-01-17 20:17:31 -0400

    adding comments

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(1, 101):
-        if i % 3 == 0 and i % 5 == 0:
+        if i % 3 == 0 and i % 5 == 0:  # if i is divisible by 3 and 5
             print("FizzBuzz", end="")
-        elif i % 3 == 0:
+        elif i % 3 == 0:  # if i is divisible by 3
             print("Fizz", end="")
-        elif i % 5 == 0:
+        elif i % 5 == 0:  # if i is divisible by 5
             print("Buzz", end="")
-        else:
-            print("{:d}".format(i), end="")
-        print(" ", end="")
+        else: # if i is not divisible by 3 or 5
+            print("{:d}".format(i), end="")  # print i
+        print(" ", end="")  # print a space
