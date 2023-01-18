0d1736c2ee25101af975aa64f3faec0dcf714d41
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-01-17 21:28:28 -0400

    alphabet in reverse

---
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+counter = 0
+alphabet = ""
+
+for i in range(ord('z'), ord('a') - 1, - 1):
+    c = chr(i)
+    if counter % 2 == 0:
+        alphabet = "{}".format(c.lower())
+    else:
+        alphabet = "{}".format(c.upper())
+    counter += 1
+    print(alphabet, end="")
