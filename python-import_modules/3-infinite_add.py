2e0d5dcd89e103b0e52468e6b085860504399273
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-01-19 14:37:42 -0400

    adding comments

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    add = 0
-    args_len = len(sys.argv)
-    for i in range(1, args_len):
-        add += int(sys.argv[i])
-    print("{:d}".format(add))
+    add = 0  # Initialize add to 0
+    args_len = len(sys.argv)  # Get the length of the arguments
+    for i in range(1, args_len):  # Loop through the arguments
+        add += int(sys.argv[i])  # Add the arguments to add
+    print("{:d}".format(add))  # Print the sum of the arguments
