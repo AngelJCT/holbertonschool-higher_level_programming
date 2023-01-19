b8f81e723a51e1e440f34430c2c55e7a877572a8
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-01-19 14:20:33 -0400

    infinite add numbers

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    add = 0
+    args_len = len(sys.argv)
+    for i in range(1, args_len):
+        add += int(sys.argv[i])
+    print("{:d}".format(add))
+    
