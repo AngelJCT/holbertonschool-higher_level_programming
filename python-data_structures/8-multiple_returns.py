d6103e0b5dd485ab4a1e4d1c5bcd313e7b84db09
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-01-23 17:22:56 -0400

    fixing edge case

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    new_tuple = ()
-    if len(sentence) == 0:
+    new_tuple = (len(sentence), sentence[0])
+    if new_tuple[0] == 0:
         sentence[0] = None
-    else:
-        new_tuple = (len(sentence), sentence[0])
-        return new_tuple
+    return new_tuple
