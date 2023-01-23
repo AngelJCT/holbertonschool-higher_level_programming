5092cc2dbde6cded1b8fa56313f1259b90eb7db9
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-01-23 18:21:36 -0400

    delete at index

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,5 +2,5 @@
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
-    my_list = [x for i, x in enumerate(my_list) if i != idx]
+    del my_list[idx]
     return my_list
