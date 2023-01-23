53e6f2f3ad40e3a8aa0c5ed3a741affb4081437c
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-01-23 09:49:16 -0400

    get element at index

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
-        return None
-    elif idx > len(my_list):
+    list_len = len(my_list)
+    if idx < 0 or idx > list_len:
         return None
     else:
         return my_list[idx]
