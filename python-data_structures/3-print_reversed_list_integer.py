c006e801077c91eec5c8fd080c43e7bd5a202cc9
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-01-23 10:49:45 -0400

    print in reversed

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if len(my_list) == 0:
-        return None
-    for i in reversed(my_list):
-        print("{:d}".format(i))
+    if my_list:
+        for i in range(len(my_list) - 1, -1, -1):
+            print("{:d}".format(my_list[i])) 
