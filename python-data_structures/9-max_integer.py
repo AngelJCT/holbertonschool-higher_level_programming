b66962a7b984616a9ffeeb21af3aa3ac3ac1a847
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-01-23 17:39:07 -0400

    return biggest integer of a list

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     sorted_list = sorted(my_list)
-    if sorted_list == "":
+    if sorted_list == 0:
         return None
     else:
         return sorted_list[-1]
