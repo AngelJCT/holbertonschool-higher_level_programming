54ccee1d305546fe2d3854fb741a293763d9500c
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-01-25 07:40:04 -0400

    fixing lambda expression and whitespace

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = []
-    lambda_func = lambda x: replace if x==search else x
-    new_list = list(map(lambda_func, my_list))
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
     return new_list
