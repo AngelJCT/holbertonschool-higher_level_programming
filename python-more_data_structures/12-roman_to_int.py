bd514cc9a3d3ae94410aa9419c8800b37540c582
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-01-26 16:01:10 -0400

    roman to integer

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_value = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
-    if roman_string == None or type(roman_string) != str:
+    if roman_string is None or type(roman_string) != str:
         return 0
     for i in range(len(roman_string)):
-        current_value = roman_value[roman_string[i]]
+        current_value = rom_val[roman_string[i]]
         if i > 0:
-            prev_value = roman_value[roman_string[i - 1]]
+            prev_value = rom_val[roman_string[i - 1]]
             if current_value > prev_value:
                 result -= 2 * prev_value
         result += current_value
