138e35ac2474f8be656951bd9dcc7cb983cabad6
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-01-27 09:11:24 -0400

    print list integers

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,6 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
                 print("{:d}".format(my_list[i]), end="")
                 count += 1
         print()
+        return count
     except:
         print()
     return count
