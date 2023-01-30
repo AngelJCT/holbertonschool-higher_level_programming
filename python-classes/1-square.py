8230da64709e10d3a37f73dcb2bfbb55b7c6e766
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-01-30 12:04:56 -0400

    initializing square class

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines a class Square"""
+
+
+class Square:
+    """This class defines a square"""
+
+    def __init__(self, size):
+        """This function initializes the square"""
+        self.__size = size
