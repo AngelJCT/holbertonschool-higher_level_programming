55454a45b6ab5aa824e746f88983027b296fe646
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-01-30 16:30:04 -0400

    fixing print statement and raise exception

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -36,7 +36,11 @@ class Square:
     @position.setter
     def position(self, value):
         """This function set the value"""
-        if type(value) is not tuple or value <= 0:
+        if type(value) is not tuple or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(value[0]) is not int or type(value[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
