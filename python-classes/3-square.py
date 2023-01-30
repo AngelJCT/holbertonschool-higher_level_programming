70f22e2a9779dac8013bfec3ab61141121e688b5
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-01-30 12:48:17 -0400

    square area of Square class

---
+++ python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module defines a Class Square"""
+
+
+class Square:
+    """This class defines a square"""
+
+    def __init__(self, size=0):
+        """This function initializes the square"""
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+    
+    def area(self):
+        """This function returns the current square area"""
+        return self.__size ** 2
