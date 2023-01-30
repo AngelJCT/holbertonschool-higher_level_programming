7ea0c3d18f018dae952509cc9b2efb936b45ec0e
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-01-30 12:41:17 -0400

    fixing order of exceptions

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -8,7 +8,7 @@ class Square:
     def __init__(self, size=0):
         """This function initializes the square"""
         self.__size = size
-        if size < 0:
-            raise ValueError("size must be >= 0")
         if type(size) is not int:
             raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
