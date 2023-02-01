ef416825701c14b2487b24734d31120ef4cc0162
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-02-01 10:24:17 -0400

    fixingraise exception

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -15,9 +15,11 @@ def print_square(size):
         ValueError: size must be >= 0
         TypeError: size must be an integer
     """
-    if not isinstance(size, int) and size < 0:
+    if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
     for _ in range(size):
         print("#" * size)
