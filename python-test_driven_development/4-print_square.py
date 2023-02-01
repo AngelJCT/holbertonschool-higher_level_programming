d064ed90153277a493eb8c6287b7616d02342ff6
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-02-01 10:18:45 -0400

    print square function and test file

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This module contains a function that prints a square"""
+
+
+def print_square(size):
+    """This function prints a square
+    Args:
+        size (int): size of the lenght of the square
+
+    Returns:
+        Nothing
+
+    Raises:
+        TypeError: size must be an integer
+        ValueError: size must be >= 0
+        TypeError: size must be an integer
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+    for _ in range(size):
+        print("#" * size)
