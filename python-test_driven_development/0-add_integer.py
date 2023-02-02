d7547ffb962678dbdcd1423e84c8426f0ae97724
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-02-02 09:04:15 -0400

    fixing edge case, code, test file

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -12,41 +12,6 @@ def add_integer(a, b=98):
 
     Raises:
         TypeError: If a or b is not an integer
-
-    >>> add_integer(1, 2)
-    3
-    >>> add_integer(100, -2)
-    98
-    >>> add_integer(1.5, 2.5)
-    4
-    >>> add_integer(1.5, 2)
-    3
-    >>> add_integer(50, "hello")
-    Traceback (most recent call last):
-        ...
-    TypeError: b must be an integer
-    >>> add_integer(50, [1, 2, 3])
-    Traceback (most recent call last):
-        ...
-    TypeError: b must be an integer
-    >>> add_integer((1, 2), 50)
-    Traceback (most recent call last):
-        ...
-    TypeError: a must be an integer
-    >>> add_integer(50, {1: 2})
-    Traceback (most recent call last):
-        ...
-    TypeError: b must be an integer
-    >>> add_integer(50, None)
-    Traceback (most recent call last):
-        ...
-    TypeError: b must be an integer
-    >>> add_integer(50, float('inf'))
-    Traceback (most recent call last):
-        ...
-    TypeError: b must be an integer
-    >>> add_integer(0, 0)
-    0
     """
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
