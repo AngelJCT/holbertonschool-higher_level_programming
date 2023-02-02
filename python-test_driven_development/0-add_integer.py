1236f5b88b68aea56653ec2b971fbe823c9a09bf
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-02-02 09:08:53 -0400

    fixing edge case, code, test file

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -15,6 +15,6 @@ def add_integer(a, b=98):
     """
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    elif type(b) is not int and type(b) is not float:
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
     return int(a) + int(b)
