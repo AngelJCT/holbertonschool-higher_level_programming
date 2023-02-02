922d57cd5a9aee8d1e30b1f34f08259dfe8c3fa2
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-02-02 13:52:00 -0400

    fix code

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -52,6 +52,6 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
-    num1 = int(a)
-    num2 = int(b)
-    return num1 + num2
+    a = int(a)
+    b = int(b)
+    return (a + b)
