e19c36247f40423bd2b3ddc75de00b51cee2826c
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-02-03 08:15:21 -0400

    fixing exceptions

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -4,35 +4,36 @@
 
 class Rectangle():
     """This class define a rectangle"""
+
     def __init__(self, width=0, height=0):
-        """This function initiate a rectangle width and height"""
-        self.__width = width
-        self.__height = height
+        """This method initialize the class Rectangle"""
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """This function gets the width"""
+        """This method returns the current width"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """This function set the value of width"""
-        if not isinstance(self.__width, int):
+        """This method sets the current width"""
+        if type(value) is not int:
             raise TypeError("width must be an integer")
-        if self.__width < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
     def height(self):
-        """This function gets the height"""
+        """This method returns the current height"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """This function set the value of height"""
-        if not isinstance(self.__height, int):
+        """This method sets the current height"""
+        if type(value) is not int:
             raise TypeError("height must be an integer")
-        if self.__height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
