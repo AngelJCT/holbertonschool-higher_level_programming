ff8c650f410f24365a3f5a4d2f43f8796e285c79
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-02-08 15:24:05 -0400

    public instance that raise an exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module 6-base_geometry"""
+
+
+class BaseGeometry():
+    """Empty class"""
+
+    def area(self):
+        """Method to raise an exception"""
+        raise Exception("area() is not implemented")
