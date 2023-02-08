874d04116acf4ee4d82c65b8bb29a3937276b5e4
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-02-08 19:15:16 -0400

    rectangle subclass

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Module 7-base_geometry"""
+
+
+class BaseGeometry():
+    """Empty class"""
+
+    def area(self):
+        """Method to raise an exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Method to validate value"""
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """Subclass derived from BaseGeometry"""
+
+    def __init__(self, width, height):
+        """Instantiation with width and height"""
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
