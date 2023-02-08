38a2ab214342d3e2bf2f59990c2c9b6f76d1c6ca
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-02-08 14:18:12 -0400

    is instance

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -4,7 +4,7 @@
 
 def is_same_class(obj, a_class):
     """Function to check if an objet is an instance of a specified class"""
-    if not isinstance(obj, a_class):
-        return False
-    else:
+    if type(obj) is a_class:
         return True
+    else:
+        return False
