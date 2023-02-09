d4bcff2134ecec5efc35c135705634c0b0d4a4a6
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-02-08 20:51:04 -0400

    adding __str__ method

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module 11-square"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Subclass of Rectangle"""
+    def __init__(self, size):
+        self.__size = size
+        super().integer_validator("size", size)
+
+    def area(self):
+        """Method that defines the area of square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """Method that prints rectangle description"""
+        return (f"[Square] {self.__size}/{self.__size}")
