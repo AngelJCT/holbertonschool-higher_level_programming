8a6e58546dcf3aee0f60061403c0641b171b9062
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-02-10 07:24:39 -0400

    adding docstring

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Module 0-read_file"""
+
+
 def read_file(filename=""):
+    """Function to read file"""
     with open(filename, encoding="utf-8") as f:
         read_file = f.read()
     print(read_file)
