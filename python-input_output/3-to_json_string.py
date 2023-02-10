9856f7bf5f31b61ffb9ab1197cb5c0225b59fc89
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-02-10 12:12:10 -0400

    json string represantation of an onject

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module 3-to_json_string"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """Return the JSON representation of an object"""
+    return json.dumps(my_obj)
