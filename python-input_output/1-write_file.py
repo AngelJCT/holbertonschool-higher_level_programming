f0f965981a5169c2b356350e64b827eee0d5aa74
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-02-10 07:59:02 -0400

    writing text to a file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module 1-write_file"""
+
+
+def write_file(filename="", text=""):
+    """Function to write text to a file"""
+    with open(filename, mode='w', encoding='utf-8') as file:
+        file.write(text)
