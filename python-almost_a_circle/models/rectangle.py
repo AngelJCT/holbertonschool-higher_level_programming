d1f321990e0bafb657127fd115d1714cddf73f79
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-02-13 11:19:41 -0400

    fixing documentation

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -17,51 +17,40 @@ class Rectangle(Base):
     @property
     def width(self):
         """Getter for width"""
-
         return self.__width
 
     width.setter
-
     def width(self, value):
         """Setter for width"""
-
         self.__width = value
 
     @property
     def height(self):
         """Getter for height"""
-
         return self.__height
 
     height.setter
 
     def height(self, value):
         """Setter for height"""
-
         self.__height = value
 
     @property
     def x(self):
         """Getter for x"""
-
         return self.__x
 
     x.setter
-
     def x(self, value):
         """Setter for x"""
-
         self.__x = value
 
     @property
     def y(self):
         """Getter for y"""
-
         return self.__y
 
     y.setter
-
     def y(self, value):
         """Setter for y"""
-
         self.__y = value
