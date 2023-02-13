61235f0b4963af9de3b0f0b4f9e9f1380c9a8124
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-02-13 11:16:33 -0400

    rectangle class

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""Module rectangle"""
+
+Base = __import__('base').Base
+
+
+class Rectangle(Base):
+    """Class Rectangle"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Constructor"""
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """Getter for width"""
+
+        return self.__width
+
+    width.setter
+
+    def width(self, value):
+        """Setter for width"""
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter for height"""
+
+        return self.__height
+
+    height.setter
+
+    def height(self, value):
+        """Setter for height"""
+
+        self.__height = value
+
+    @property
+    def x(self):
+        """Getter for x"""
+
+        return self.__x
+
+    x.setter
+
+    def x(self, value):
+        """Setter for x"""
+
+        self.__x = value
+
+    @property
+    def y(self):
+        """Getter for y"""
+
+        return self.__y
+
+    y.setter
+
+    def y(self, value):
+        """Setter for y"""
+
+        self.__y = value
