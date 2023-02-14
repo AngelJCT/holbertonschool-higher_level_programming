3f36dc6e2d8cb5ea1d3bb7e0facb7e48d85c78d0
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-02-14 14:57:40 -0400

    adding json string repr method

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,6 +2,8 @@
 """Module 0-base.py"""
 
 
+import json
+
 class Base:
     """Class Base"""
     __nb_objects = 0
@@ -13,3 +15,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+    
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return Json string representation"""
+        if list_dictionaries is None:
+            return "[]"
+        return json.dumps(list_dictionaries)
