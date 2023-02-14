1e6610f66554e20136639b33ee3e77c916adf1e7
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-02-14 17:04:49 -0400

    fixing amount of arguments

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -43,7 +43,7 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
         """Return an instance with all attributes already set"""
-        dummy_instance = cls(1, 1)
+        dummy_instance = cls(1, 1, 2, 3, 4)
         dummy_instance.update(**dictionary)
         return dummy_instance
 
