90e3690138504f3e4237fd9c54fa1d09b73898fb
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-02-15 16:42:35 -0400

    adding edge case for rectangle

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -60,8 +60,7 @@ class TestBase(unittest.TestCase):
         with open("Rectangle.json", "r") as f:
             self.assertEqual(f.read(), '[{"id": 1, "width": 10, "height": 7, "x": 2, "y": 8}, {"id": 2, "width": 2, "height": 4, "x": 0, "y": 0}]')
         with self.assertRaises(TypeError):
-            Rectangle.save_to_file()
-
+            Rectangle.save_to_file(r1)
 
 
 if __name__ == '__main__':
