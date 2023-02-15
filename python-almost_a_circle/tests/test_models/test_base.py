93471231d159a1f52e66d86b4c751825f3b8af44
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-02-15 14:30:28 -0400

    fixing description

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -3,18 +3,22 @@
 
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+import json
+import os
 
 
 class TestBase(unittest.TestCase):
     """Test cases for Base class"""
 
-    def test_init_withouth_id(self):
-        """Test case for initialization with id"""
+    def test_init_without_id(self):
+        """Test case for initialization without id"""
         b1 = Base()
         self.assertEqual(b1.id, 1)
 
     def test_init_with_id(self):
-        """Test case for initialization without id"""
+        """Test case for initialization with id"""
         b2 = Base(40)
         self.assertEqual(b2.id, 40)
 
