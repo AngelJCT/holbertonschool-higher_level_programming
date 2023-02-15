af7e83e23438cb046396f991133dcc9297ca7331
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-02-15 16:04:54 -0400

    adding a copy to test folder

---
+++ python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""Unittest for Base class"""
+
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+import json
+import os
+
+
+class TestBase(unittest.TestCase):
+    """Test cases for Base class"""
+
+    def setUp(self):
+        """Set up for test cases"""
+        Base._Base__nb_objects = 0
+
+    def test_id_none(self):
+        """Test case for initialization without id"""
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+
+    def test_id(self):
+        """Test case for initialization with id"""
+        b2 = Base(40)
+        self.assertEqual(b2.id, 40)
+
+    def test_nb_objects(self):
+        """Test case for initialization without id and increment"""
+        b1 = Base()
+        b2 = Base()
+        b3 = Base()
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+        self.assertEqual(b3.id, 3)
+
+    def test_to_json_string_none(self):
+        """Test case for to_json_string with None"""
+        self.assertEqual(Base.to_json_string(None), "[]")
+
+    def test_to_json_string(self):
+        """Test case for to_json_string"""
+        r1 = Rectangle(10, 7, 2, 8)
+        dictionary = r1.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+        self.assertEqual(json_dictionary, '[{"id": 1, "width": 10, "height": 7, "x": 2, "y": 8}]')
+
+    def test_save_to_file_none(self):
+        """Test case for save_to_file with None"""
+        Base.save_to_file(None)
+        with open("Base.json", "r") as f:
+            self.assertEqual(f.read(), "[]")
+
+    def test_save_to_file(self):
+        """Test case for save_to_file"""
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        Rectangle.save_to_file([r1, r2])
+        with open("Rectangle.json", "r") as f:
+            self.assertEqual(f.read(), '[{"id": 1, "width": 10, "height": 7, "x": 2, "y": 8}, {"id": 2, "width": 2, "height": 4, "x": 0, "y": 0}]')
+
+
+if __name__ == '__main__':
+    unittest.main()
