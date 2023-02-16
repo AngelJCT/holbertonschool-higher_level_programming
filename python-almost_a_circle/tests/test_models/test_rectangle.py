ca4805b04ad754144ecfc52ad0c679ad6310dd2e
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-02-16 08:04:37 -0400

    adding test rectangle

---
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""Unittest for Rectangle class"""
+
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+import json
+import os
+
+
+class TestRectangle(unittest.TestCase):
+    """Test cases for Rectangle class"""
+
+    def setUp(self):
+        """Set up for test cases"""
+        Base._Base__nb_objects = 0
+
+    def test_rectangle(self):
+        """Test case for non-list arguments"""
+        r1 = Rectangle(10, 2)
+        self.assertEqual(r1.id, 1)
+        self.assertEqual(r1.width, 10)
+        self.assertEqual(r1.height, 2)
+        r3 = Rectangle(1, 2, 3, 4, 5)
+        self.assertEqual(r3.id, 5)
+        self.assertEqual(r3.width, 1)
+        self.assertEqual(r3.height, 2)
+        self.assertEqual(r3.x, 3)
+        self.assertEqual(r3.y, 4)
+
+    def test_rectangle_with_negative_arguments(self):
+        """Test case for negative arguments"""
+        self.assertRaises(ValueError, Rectangle, 10, -2)
+        self.assertRaises(ValueError, Rectangle, -10, 2)
+        self.assertRaises(ValueError, Rectangle, 10, 2, -3)
+        self.assertRaises(ValueError, Rectangle, 10, 2, 3, -4)
+
+    def test_rectangle_with_zero_arguments(self):
+        """Test case for zero arguments"""
+        self.assertRaises(ValueError, Rectangle, 10, 0)
+        self.assertRaises(ValueError, Rectangle, 0, 10)
+
+    def test_rectangle_with_non_list_argument(self):
+        """Test case for non-list arguments"""
+        self.assertRaises(TypeError, Rectangle, 2, "10")
+
+    def test_area(self):
+        """Test case for area method"""
+        r1 = Rectangle(1, 2)
+        self.assertEqual(r1.area(), 2)
+
+
+if __name__ == "__main__":
+    unittest.main
