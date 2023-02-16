87927b45f25ea5f04364b9f9a44e39d64c6b516e
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-02-16 19:24:23 -0400

    pascal triangle excercise

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr.bin/python3
+"""Module 12-pascal_triangle"""
+
+
+def pascal_triangle(n):
+    """Function to print a pascal triangle"""
+    if n <= 0:
+        return []
+    if n == 1:
+        return [[1]]
+    if n == 2:
+        return [[1], [1, 1]]
+    triangle = [[1], [1, 1]]
+    for i in range(2, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
