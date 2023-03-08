729630ecc208e9fe16b5d028328a998c2d2a338e
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-03-08 16:10:00 -0400

    creating a second table

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,12 +1,3 @@
 -- create a second table and add multiple rows
-CREATE TABLE IF NOT EXISTS second_table (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(256) NOT NULL,
-    score INT
-    PRIMARY KEY (id)
-);
-INSERT INTO second_table (id, name, score) VALUES
-    (1, 'John', 10),
-    (2, 'Alex', 3),
-    (3, 'Bob', 14),
-    (4, 'George', 8);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
