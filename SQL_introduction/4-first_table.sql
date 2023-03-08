066e911a160b18276509d8cbc9abe8e35b449a3a
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-03-08 14:11:29 -0400

    creating a table in the database

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,6 @@
 -- create a table in the current database
--- first column: id with datatype and set as primary key that will auto-increment with each new record added
--- second column: name withe length of 256 chars with NOT NULL constraint to ensure that must have a value
+USE [hbtn_0c_0]
+
 CREATE TABLE IF NOT EXISTS first_table (
     id INT,
     name VARCHAR(256),
