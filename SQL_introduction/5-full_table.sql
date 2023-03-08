9f97f8d6328f7556d5b82c51df93d9990ee82596
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-03-08 14:57:51 -0400

    print full description of a table

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,8 @@
 -- print the full description of the first_table from database hbtn_0c_0 in MySQL server
 SELECT
-    *
+    id, name,
+    INT, VARCHAR(256),
+    IS NULLABLE,
+    KEY,
 FROM
     first_table;
