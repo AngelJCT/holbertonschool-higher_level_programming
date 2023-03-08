a669e49e00849a7d12ab3b2ef95b64d0ca6dd355
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-03-08 15:58:28 -0400

    creating a second table

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- create a second table and add multiple rows
-CREATE TABLE second_table (
+CREATE TABLE IF NOT EXISTS second_table (
     id INT NOT NULL PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(256) NOT NULL,
     score INT
