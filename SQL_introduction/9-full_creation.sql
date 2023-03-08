f336748b5c35c45a61da13064d03afbc24b99a6a
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-03-08 16:02:37 -0400

    creating a second table

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -4,3 +4,8 @@ CREATE TABLE IF NOT EXISTS second_table (
     name VARCHAR(256) NOT NULL,
     score INT
 );
+INSERT INTO second_table (name, score) VALUES
+    (1, 'John', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
+    (4, 'George', 8);
