8a26bec7107c57858e93c485cb16d13c38051232
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-03-09 07:01:08 -0400

    list all records

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- list all records of the table second_table
-SELECT score(*), name(*) FROM second_table ORDER BY score DESC, name DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL OR IS NULL ORDER BY score DESC, name DESC;
