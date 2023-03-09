c618344baec7844798c6c5c82e233bcd2cecde15
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-03-09 06:33:32 -0400

    list records with same score

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- list the number of records with the same score in second_table
-SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC, name DESC;
+SELECT score, COUNT(score) AS number FROM second_table ORDER BY score DESC, name DESC GROUP BY score;
