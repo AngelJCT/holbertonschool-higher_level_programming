e030d527f3f59f2090fe5f2a16cc85e0575f2b9c
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-03-10 10:29:00 -0400

    show users privileges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
 -- list all privileges of the MySQL users user_0d_1 and user_0d_2 on my server(localhost)
-SHOW GRANTS FOR user_0d_1, user_0d_2 @ localhost;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
