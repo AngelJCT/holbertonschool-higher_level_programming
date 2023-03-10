76e146421921de6888357e1f961f7d79c9c67e6e
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-03-10 10:37:37 -0400

    creating user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- create MySQL server for user_0d_1
 CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
