5797cad91ffde9d8837c076c4e7646268dfafaca
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-03-10 20:18:52 -0400

    genres and number of shows

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
-SELECT genres.name, COUNT(tv_show_genres.genre_id) AS number_of_shows
+SELECT tv_genres.name, COUNT(tv_show_genres.genre_id) AS number_of_shows
 FROM genres
 LEFT JOIN tv_show_genres
-ON genres.id = tv_show_genres.genre_id
-GROUP BY genres.name
-ORDER BY num_shows DESC, genres.name ASC;
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY num_shows DESC, tv_genres.name ASC;
