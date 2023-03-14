6ad06155faad8b5d700dd1f00eca8feef0b68766
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-03-14 11:02:36 -0400

    slecting name that matches the argument

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module 0-select_states"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    """Main function"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database,
+        state=state_name
+        )
+    cur = db.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE name = 'state' ORDER BY id ASC"
+        )
+    for row in cur:
+        print(row)
