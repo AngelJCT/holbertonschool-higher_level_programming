6c058a7ab22d1803448e7c5aa3073cc01f5db2ec
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-03-14 10:00:03 -0400

    first commit

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,14 @@
+"""Module 0-select_states"""
+import MySQLdb
+
+def main():
+    """Main function"""
+    db = MySQLdb.connect(host="localhost", port=3306, user="root",
+                            passwd="root", db="hbtn_0e_0_usa")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    db.close()
