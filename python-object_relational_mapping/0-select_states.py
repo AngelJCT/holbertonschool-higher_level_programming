4192d1cd8831da7633ad83916938043ed9ff4fcd
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-03-14 10:05:58 -0400

    fixing code

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,12 +2,12 @@
 """Module 0-select_states"""
 import MySQLdb
 
+
 def main():
     """Main function"""
     db = MySQLdb.connect(host="localhost", port=3306, user="root",
-                            passwd="root", db="hbtn_0e_0_usa")
+                        passwd="root", db="hbtn_0e_0_usa")
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
-    query_rows = cur.fetchall()
-    for row in query_rows:
+    for row in cur:
         print(row)
