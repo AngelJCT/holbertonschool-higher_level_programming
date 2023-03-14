9ee23c305618573ee6737affd0c0ca4a1266da4a
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-03-14 18:15:51 -0400

    filtering cities

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Module 5-filter_cities"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    """Lists all cities from database hbtn_0e_4_usa"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+        )
+    cur = db.cursor()
+    query = "SELECT cities.name FROM cities\
+            INNER JOIN states ON cities.state_id = states.id\
+            WHERE states.name = %s\
+            ORDER BY cities.id ASC"
+    cur.execute(query, (state,))
+    for row in cur:
+        print(row[0], end=", ")
