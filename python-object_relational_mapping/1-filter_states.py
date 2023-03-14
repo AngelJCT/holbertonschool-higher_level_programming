d9c2a7aea4c7d05e7bfaac7bcc685d3ffd034c1d
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-03-14 10:29:41 -0400

    filtering names of states

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module 0-select_states"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    """Main function"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+        )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    for row in cur:
+        print(row)
