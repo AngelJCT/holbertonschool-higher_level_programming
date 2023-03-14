275d6b6f8f499e7f3ba14bb1c8fe3f5e72ad7491
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-03-14 11:23:45 -0400

    fixing format for user input and pycodestyle

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -20,7 +20,8 @@ if __name__ == "__main__":
         db=database
         )
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE name LIKE '{}' ORDER BY id ASC".format(state_name)
+    query = "SELECT * FROM states WHERE name LIKE '{}'\
+        ORDER BY id ASC".format(state_name)
     cur.execute(query)
     for row in cur:
         print(row)
