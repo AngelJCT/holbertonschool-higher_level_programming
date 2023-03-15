a402601a1931d1126f9a705f506be4d04307cda8
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-03-14 23:06:33 -0400

    lists State objects

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module 7-model_state_fetch_all.py"""
+
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+if __name__ == "__main__":
+    """lists all State objects from database hbtn_0e_06_usa"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine(
+        f"mysql+mysqldb://{username}:{password}@localhost/{database}",
+        pool_pre_ping=True,
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state in session.query(State).order_by(State.id):
+        print(f"{state.id}: {state.name}")
+    session.close()
