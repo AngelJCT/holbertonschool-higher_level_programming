70519641504c9f6604e437f139a8a6c1e2beaece
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-03-15 15:50:50 -0400

    filter state with a

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -18,6 +18,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     states = session.query(State).filter(State.name.like("%a%")).order_by(State.id)
-    for state in states:
+    results = states.all()
+    for state in results:
         print(f"{state.id}: {state.name}")
     session.close()
