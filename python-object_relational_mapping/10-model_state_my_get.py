1f34d766d7d17d0e88598b187e6f4f6d5304350b
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-03-15 16:35:49 -0400

    state name = passed argument

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module 10-model_state_my_get"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+if __name__ == "__main__":
+    """Print the State object with the name passed as argument"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    engine = create_engine(
+        f"mysql+mysqldb://{username}:{password}@localhost/{database}"
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    names = session.query(State.name.like(f"%{state_name}%"))
+    if names:
+        print(f"{names.id}")
+    else:
+        print("Not found")
+    session.close()
