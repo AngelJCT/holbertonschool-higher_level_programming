bdefa17b4833b18ecd5a610f92072f0ba97134a6
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-03-16 06:54:59 -0400

    deleting states with a in it

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module 13-model_delete_a"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+
+if __name__ == "__main__":
+    """Delete all State objects with a name containing the letter a"""
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    engine = create_engine(
+        f"mysql+mysqldb://{username}:{password}@localhost/{database}"
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    if state := session.query(State).filter(State.name.like("%a%")):
+        session.delete(state)
+        session.commit()
+    session.close()
