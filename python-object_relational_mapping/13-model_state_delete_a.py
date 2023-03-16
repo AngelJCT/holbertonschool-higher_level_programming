bf3229f867f9f594aa40a4b0bfa863a7c09829e5
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-03-16 07:07:21 -0400

    deleting states with a in it

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -17,6 +17,6 @@ if __name__ == "__main__":
     )
     Session = sessionmaker(bind=engine)
     session = Session()
-    session.query(State).filter(State.name.like("%a%")).delete()
+    session.query(State).filter(State.name.like("%a%")).delete(synchronize_session=False)
     session.commit()
     session.close()
