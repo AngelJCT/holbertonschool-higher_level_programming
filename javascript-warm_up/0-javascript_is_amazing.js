4d12c3bbbba1f9ef15350e998b69490e7a27aed7
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-04-17 11:50:42 -0400

    fixing this ugly language

--- javascript-warm_up/0-javascript_is_amazing.js
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 function message () {
-    let myVar = 'JavaScript is amazing';
-    console.log(myVar);
+  const myVar = 'JavaScript is amazing';
+  console.log(myVar);
 }
 message();
