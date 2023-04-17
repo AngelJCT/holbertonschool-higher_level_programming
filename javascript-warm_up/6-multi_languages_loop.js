29d797c2ccc8ad70543e5f0398a8aeca06ee358a
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-04-17 16:35:38 -0400

    print three lines using a for loop and on console.log

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -2,7 +2,7 @@
 // prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
 
 function multiLanguages2 () {
-  const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+  const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
   for (let i = 0; i < array.length; i++) {
     console.log(array[i]);
   }
