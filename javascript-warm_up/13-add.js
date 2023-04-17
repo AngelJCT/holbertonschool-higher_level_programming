c43ea49c94f79dcf60d18b2796838f0d896f99d6
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-04-17 18:04:11 -0400

    exporting add function

---
+++ javascript-warm_up/13-add.js
@@ -0,0 +1,8 @@
+#!/usr/local/bin/node
+// prints the addition of 2 integers
+// The function must be visible from outside
+// The name of the function must be add
+
+exports.add = function (a, b) {
+  return a + b;
+}
