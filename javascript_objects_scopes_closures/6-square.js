c7ae995c6ffe5151d51353a90343e8a448677f05
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-04-18 11:08:26 -0400

    square #6

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -5,21 +5,21 @@ class Square extends Square1 {
   constructor (size) {
     super(size, size);
   }
-  
+
   charPrint (c) {
     if (c === undefined) {
-        this.print();
+      this.print();
     } else {
-        let output = '';
-        for (let i = 0; i < this.height; i++) {
-          for (let j = 0; j < this.width; j++) {
-            output += c;
-          }
-          if (i < this.height - 1) {
-            output += '\n';
-          }
+      let output = '';
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) {
+          output += c;
         }
-        console.log(output);
+        if (i < this.height - 1) {
+          output += '\n';
+        }
+      }
+      console.log(output);
     }
   }
 }
