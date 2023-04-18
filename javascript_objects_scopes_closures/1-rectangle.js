67212b039ac6b64852608c2e8e4fd884c5522bc5
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-04-17 20:39:04 -0400

    class rectangle

---
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Class Rectangle that defines a rectangle
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
