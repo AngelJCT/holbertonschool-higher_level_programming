f691ca7a98016804e90e3030201711ee1f3c3722
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-04-17 22:52:33 -0400

    rectangle #2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -3,7 +3,7 @@
 class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
-    return;
+      return;
     }
     this.width = w;
     this.height = h;
