76f14c517cce9cb7ae2bf53cd90246d2920b7036
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-04-18 17:06:19 -0400

    convert from base 10 to another base

---
+++ javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// function to convert a number from base 10 to another base passed as argument
+exports.converter = function (base) {
+  return function (num) { // returns another function which takes 'num' as argument
+    return num.toString(base); // inner function that uses 'toString' method to convert the number to a string representation in the specified base
+  };
+}
