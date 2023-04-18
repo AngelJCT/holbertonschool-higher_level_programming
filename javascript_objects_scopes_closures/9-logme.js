27f072bef00397cd6dcd333324e768ebcbf7e680
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-04-18 16:19:59 -0400

    print number of arguments

---
+++ javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,9 @@
+#!/usr/local/bin/node
+// function to prints the number of arguments already printed and the new argument value
+exports.logMe = function (item) {
+  if (this.count === undefined) {
+    this.count = 0;
+  }
+  console.log(this.count + ': ' + item);
+  this.count++;
+};
