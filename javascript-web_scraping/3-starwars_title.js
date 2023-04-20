bb187fd25cd1d005ec29927fe5fe7b99b21a47d9
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-04-20 14:54:48 -0400

    prints the title of a star wars movie

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -3,9 +3,9 @@
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request.get(url, (error, response, body) => {
-    if (error) {
-        console.error(error);
-        return;
-    }
-    console.log(JSON.parse(body).title);
+  if (error) {
+    console.error(error);
+    return;
+  }
+  console.log(JSON.parse(body).title);
 });
