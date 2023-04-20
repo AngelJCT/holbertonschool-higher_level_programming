a1660e2cfc4afef136806f303cf15ee325f01c1e
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-04-20 11:28:20 -0400

    witing to a file

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/local/bin/node
+// write a script that writes a string to a file
+const fs = require('fs');
+const file = process.argv[2];
+const data = process.argv[3];
+fs.writeFile(file, data, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+});
