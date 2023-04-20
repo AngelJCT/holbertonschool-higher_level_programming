6890b72e520e7c4ad2d577dbdc0ae7ef662df4f7
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-04-20 11:29:47 -0400

    witing to a file

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -4,8 +4,7 @@ const fs = require('fs');
 const file = process.argv[2];
 const data = process.argv[3];
 fs.writeFile(file, data, (err) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
+  if (err) {
+    console.error(err);
+  }
 });
