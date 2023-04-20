ccdc8436eeee17767214bad36754b3451537b448
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-04-20 15:56:58 -0400

    from webpage to file"

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -5,13 +5,13 @@ const fs = require('fs');
 const url = process.argv[2];
 const filePath = process.argv[3];
 request.get(url, (error, response, body) => {
-    if (error) {
-        console.error(error);
-        return;
+  if (error) {
+    console.error(error);
+    return;
+  }
+  fs.writeFile(filePath, body, (err) => {
+    if (err) {
+      console.error(err);
     }
-    fs.writeFile(filePath, body, (err) => {
-        if (err) {
-            console.error(err);
-        }
-    });
+  });
 });
