e042a0ba1401c785a7ab3f9d300e4d56f7f8087a
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-04-20 15:12:25 -0400

    count movies of Wedge Antilles

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -2,6 +2,9 @@
 // script that prints the number of movies where the character “Wedge Antilles” is present.
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/';
+const characterId = '18';
+const characterUrl = 'https://swapi-api.hbtn.io/api/people/' + characterId;
+
 request.get(url, (error, response, body) => {
   if (error) {
     console.error(error);
@@ -10,12 +13,9 @@ request.get(url, (error, response, body) => {
   const films = JSON.parse(body).results;
   let count = 0;
   for (const film of films) {
-    for (const character of film.characters) {
-      if (character.includes('18')) {
-        count++;
-      }
+    if (film.characters.includes(characterUrl)) {
+      count++;
     }
   }
   console.log(count);
-}
-);
+});
