5ae2ded5199c5dc16cb9cc3ddbf25a063308665f
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-04-20 14:40:46 -0400

    status code

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// script that display the status code of a GET request
+const request = require('request');
+const url = process.argv[2];
+request.get(url, (error, response) => {
+    if (error) {
+        console.error(error);
+        return;
+    }
+    console.log("code: ${response.statusCode}")
+});
