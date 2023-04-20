960254fc0fd74be30fd50a229077cfda895f6577
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-04-20 16:06:33 -0400

    completed tasks

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+// script that computes the number of tasks completed by user id.
+const request = require('request');
+const url = process.argv[2];
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  const tasks = JSON.parse(body).results;
+  const completedTasks = {};
+  tasks.forEach(task => {
+    if (task.completed === true) {
+      if (completedTasks[task.userId] === undefined) {
+        completedTasks[task.userId] = 1;
+      } else {
+        completedTasks[task.userId]++;
+      }
+    }
+  }
+  );
+  console.log(completedTasks);
+});
