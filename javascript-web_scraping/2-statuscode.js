b42d43f35669d3cb41e1a45b664625e1d54fdde2
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-04-20 14:45:47 -0400

    status code

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -7,5 +7,5 @@ request.get(url, (error, response) => {
         console.error(error);
         return;
     }
-    console.log("code: ",response.statusCode)
+    console.log("code:",response.statusCode)
 });
