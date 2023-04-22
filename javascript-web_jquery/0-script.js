760b11e324532a3a2414360794ca8be269f08618
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-04-22 10:39:18 -0400

    first commit

--- javascript-web_jquery/0-script.js
+++ javascript-web_jquery/0-script.js
@@ -1,3 +1,4 @@
 #!/usr/bin/node
 // script that updates the text color of the HTML tag HEADER to red (#FF0000):
-$('header').css('color', '#FF0000');
+const header = document.querySelector("header");
+header.style.color = "#FF0000";
